fab84ab3ba711dd494c601f15e4f7fa60b01bffc
Pedro Alfaro <pedro.alfaro@co-modality.com>
2026-05-14 12:08:23 +0200

    fix reporting module

--- src/reporting/pdf_report.py
+++ src/reporting/pdf_report.py
@@ -7,12 +7,10 @@ from reportlab.platypus import (
 from reportlab.lib.styles import getSampleStyleSheet
 from reportlab.lib.pagesizes import letter
 
-import os
-
 
 def generate_executive_pdf(row, summary):
 
-    output_path = f"temp_{row['company']}.pdf"
+    output_path = f"{row['company']}_report.pdf"
 
     doc = SimpleDocTemplate(
         output_path,
@@ -23,9 +21,7 @@ def generate_executive_pdf(row, summary):
 
     elements = []
 
-    title = f"""
-    WindEurope Executive Intelligence Report
-    """
+    title = "WindEurope Executive Intelligence Report"
 
     elements.append(
         Paragraph(title, styles['Title'])
@@ -40,8 +36,6 @@ def generate_executive_pdf(row, summary):
     <b>Country:</b> {row['country']}<br/>
     <b>Strategic Level:</b> {row['strategic_level']}<br/>
     <b>Lead Tier:</b> {row['lead_tier']}<br/>
-    <b>Lead Score:</b> {row['lead_score']}<br/>
-    <b>Strategic Score:</b> {row['strategic_score']}<br/>
     """
 
     elements.append(
@@ -59,19 +53,6 @@ def generate_executive_pdf(row, summary):
 
     elements.append(
         Paragraph(executive_summary, styles['BodyText'])
-    )
-
-    elements.append(
-        Spacer(1, 20)
-    )
-
-    sales_strategy = f"""
-    <b>AI Sales Strategy</b><br/><br/>
-    {row['sales_strategy']}
-    """
-
-    elements.append(
-        Paragraph(sales_strategy, styles['BodyText'])
     )
 
     doc.build(elements)
